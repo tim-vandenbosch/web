e3162c56c921af73a706ff086f21ccc90fa40462
Marnix <marnix.meuwissen@student.pxl.be>
2016-04-26 15:56:02 +0200

    Gebruiken van Template

---
+++ web2/application/views/Gebruikers/Beheerder/BeheerderHome.php
@@ -0,0 +1,33 @@
+<?php
+ /**
+    * Gebruiker: Marnix
+    * Datum: 26/04/2016
+    * Bron: /
+    */
+?>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <link href="../bootstrap/css/bootstrap.css" rel="stylesheet"/>
+        <link href="../bootstrap/css/customStyle.css" rel="stylesheet"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body id="background">
+        <header class="container hoofding">
+            <div id="pxlLine"></div>
+            <img class="navbar-header" src="../Pictures/Logo_PXL.png" alt="PXL logo"/>
+            <nav class="nav">
+                <ul class="nav navbar-nav navbar-style">
+                    <li class="active"><a href="TemplateHome.html">Home</a></li>
+                    <li><a href="#">Testscreen</a></li>
+                </ul>                 
+            </nav>
+            <div class="col-md-2"></div>
+        </header>
+        <div class="col-lg-12 main home">
+            
+        </div>   
+        
+    </body>
+</html>
