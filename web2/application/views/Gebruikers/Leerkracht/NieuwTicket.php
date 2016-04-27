13122ec7acf24459dbcb4ef92d7a5f560e9c48a6
Marnix <marnix.meuwissen@student.pxl.be>
2016-04-27 12:28:15 +0200

    aanmaken new Ticket view

---
+++ web2/application/views/Gebruikers/Leerkracht/NieuwTicket.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <title>Nieuw Ticket</title>
+        <link href="../../bootstrap/css/bootstrap.css" rel="stylesheet"/>
+        <link href="../../bootstrap/css/customStyle.css" rel="stylesheet"/>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body id="background">
+        <header class="container hoofding">
+            <div id="pxlLine"></div>
+            <img class="navbar-header" src="../../Pictures/Logo_PXL.png" alt="PXL logo"/>
+            <nav class="nav">
+                <ul class="nav navbar-nav navbar-style">
+                    <li class="active"><a href="TemplateHome.html">Overzicht</a></li>
+                    <li><a href="#">Nieuw Ticket</a></li>
+                    <li><a href="#">Profiel</a></li>
+                    <li><a href="#">Afmelden</a></li>
+                </ul>                 
+            </nav>
+            <div class="col-md-2"></div>
+        </header>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 titel">
+                <h1>Nieuw Ticket</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 main home ">
+                
+            
+            </div>   
+        </div>
+    </body>
+</html>
+
