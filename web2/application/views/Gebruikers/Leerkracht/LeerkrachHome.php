cd4a8ee43e8a86b821c8c3c2e9fec5ab9a3366aa
Marnix <marnix.meuwissen@student.pxl.be>
2016-04-27 12:14:08 +0200

    update van codeigniter views

--- web2/application/views/Gebruikers/Leerkracht/LeerkrachHome.php
+++ web2/application/views/Gebruikers/Leerkracht/LeerkrachHome.php
@@ -8,26 +8,45 @@
 <html>
     <head>
         <title>TODO supply a title</title>
-    <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-    <link href="<?php echo base_url();?>/assets/bootstrap/css/customStyle.css" rel="stylesheet"/>
+        <link href="../../bootstrap/css/bootstrap.css" rel="stylesheet"/>
+        <link href="../../bootstrap/css/customStyle.css" rel="stylesheet"/>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body id="background">
         <header class="container hoofding">
             <div id="pxlLine"></div>
-            <img class="navbar-header" src="<?php echo base_url();?>assets/Pictures/Logo_PXL.png" alt="PXL logo"/>
+            <img class="navbar-header" src="../../Pictures/Logo_PXL.png" alt="PXL logo"/>
             <nav class="nav">
                 <ul class="nav navbar-nav navbar-style">
-                    <li class="active"><a href="TemplateHome.html">Home</a></li>
-                    <li><a href="#">Testscreen</a></li>
+                    <li class="active"><a href="TemplateHome.html">Overzicht</a></li>
+                    <li><a href="#">Nieuw Ticket</a></li>
+                    <li><a href="#">Profiel</a></li>
+                    <li><a href="#">Afmelden</a></li>
                 </ul>                 
             </nav>
             <div class="col-md-2"></div>
         </header>
-        <div class="col-lg-12 main home">
-            
-        </div>   
-        
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 titel">
+                <h1>Overzicht</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 main home ">
+                <div class="col-md-4 left">
+                    <h2>Tickets</h2>
+                </div>
+                <div class="col-md-8 right ">
+                    <div>LALALALALA tekst</div>
+                </div>
+            </div>   
+        </div>
+        <div class="row">
+            <div class="col-md-12 center">
+                <!--hier moet de table komen-->
+            </div>
+        </div>
     </body>
 </html>
+
