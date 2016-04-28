9566ab68772b8d715837fc442e5b56fb8e43862d
hitocage <briittv@gmail.com>
2016-04-28 09:41:46 +0200

    elementen bijwerken

--- web2/assets/bootstrap/js/validatieLoginForm.js
+++ web2/assets/bootstrap/js/validatieLoginForm.js
@@ -4,18 +4,16 @@
  * Bron: /
  */
 function validateEmail() {
-    var x = document.getElementById("email").value;
+    var x = document.getElementById("user").value;
     var atpos = x.indexOf("@");
     var dotpos = x.lastIndexOf(".");
     if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
         alert("Not a valid e-mail address");
-        document.getElementById("email").value = " ";
-        //return false;
         event.preventDefault();
     } else
         alert("valid en sent");
 }
 
 function validatew8w() {
-    
+
 }
