ef44e0d512d18d099f8f9356904ae1c26f6139f2
hitocage <briittv@gmail.com>
2016-04-28 09:40:30 +0200

    validatie maken

---
+++ web2/assets/bootstrap/js/validatieLoginForm.js
@@ -0,0 +1,21 @@
+/**
+ * Gebruiker: Britt
+ * Datum: 28/04/2016
+ * Bron: /
+ */
+function validateEmail() {
+    var x = document.getElementById("email").value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        document.getElementById("email").value = " ";
+        //return false;
+        event.preventDefault();
+    } else
+        alert("valid en sent");
+}
+
+function validatew8w() {
+    
+}
