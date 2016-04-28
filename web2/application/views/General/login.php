06f28ca4e9287d7ac2433a5f5eb0a76021ef6cd6
hitocage <briittv@gmail.com>
2016-04-28 10:25:20 +0200

    toevoegen van de source javascript

--- web2/application/views/General/login.php
+++ web2/application/views/General/login.php
@@ -7,6 +7,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+<script src="/assets/bootstrap/js/validatieLoginForm.js"></script>
+
 <body id="background">
 <header class="container hoofding">
     <div id="pxlLine"></div>
