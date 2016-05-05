e58b4fc82bafe4bec520747e206f97163782e33c
hitocage <briittv@gmail.com>
2016-05-05 12:43:52 +0200

    functie voor rol op te halen in model LOGIN

--- website/application/models/user.php
+++ website/application/models/user.php
@@ -7,10 +7,30 @@ Class User extends CI_Model
 {
     function login($email,$password)
     {
-        $this->db->select('userID, email, pws, rol');
+        $this->db->select('userID, email, pws');
         $this->db->from('users');
         $this->db->where('email',$email);
         $this->db->where('pws',MD5($password));
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        // Als de gebruiker gevonden wordt in de db
+        if($query -> num_rows() == 1)
+        {
+            return $query->result();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    function neem_rol($email)
+    {
+        $this->db->selec('email, rol');
+        $this->db->from('users');
+        $this->db->where('email', $email);
         $this->db->limit(1);
 
         $query = $this->db->get();
