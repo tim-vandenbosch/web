1c6a496c6cd8a3e83a6dedce5aeadc2ef84aae18
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-09 15:43:34 +0200

    update Ticketsmodel

--- website/application/models/ticket.php
+++ website/application/models/ticket.php
@@ -7,9 +7,27 @@
  */
 Class Ticket extends CI_Model{
 
-    function getUserTickets($userId){
+    function getUserTickets($userID){
 
-        $query = $this -> db -> get_where('tickets',array('aanmaker'=>$userId));
+        $this -> db -> select('ticketID,onderwerp,prioriteit,type,campusID,blokID,lokaalNummer,herstellingsDatum,status');
+        $this -> db -> from('tickets');
+        $this -> db -> where('aanmaker',$userID);
+
+        $query = $this -> db -> get();
+
+        // Als er geen tickets gevonden worden in de db
+        if($query -> num_rows() == 1)
+        {
+            return $query->result();
+        }
+        else
+        {
+            return false;
+        }
+    }
+    function getAllTickets(){
+
+        $query = $this -> db -> get('tickets');
 
         // Als er tickets gevonden worden in de db
         if($query -> num_rows() >= 1)
@@ -21,6 +39,5 @@ Class Ticket extends CI_Model{
             return false;
         }
     }
-
 
 }
