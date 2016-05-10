d09dd3b7cea9bfa80bd99bf7fc6369ee9507581a
nida-ilyas <nida.ilyas@student.pxl.be>
2016-05-10 18:00:08 +0200

    Commitje

---
+++ website/application/views/Werkman/index.php
@@ -0,0 +1,24 @@
+<div class="container">
+    <h2>Condensed Table</h2>
+
+
+    <table class="table table-condensed">
+        <thead>
+        <tr>
+            <th>Type</th>
+            <th>Onderwerp</th>
+            <th>Aanmaak datum</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        <tr>
+            <td><?php echo $type;?></td>
+            <td><?php echo $onderwerp;?></td>
+            <td><?php echo $aanmaakDatum;?></td>
+        </tr>
+
+        </tbody>
+    </table>
+
+</div>
