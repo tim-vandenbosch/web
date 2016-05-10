5d245644c11f57759b25210a1d9882d41ec4643d
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-10 09:10:03 +0200

    newTicketview maken

--- website/application/views/newTicket_view.php
+++ website/application/views/newTicket_view.php
@@ -33,7 +33,43 @@
     <div class="col-md-2"></div>
 </header>
 <div class="col-lg-12 main home">
-    <!-- HIER MOET JE CODE -->
+    <form>
+        <fieldset>
+            <label>ID</label><label id="ticketId" name="ticketId"></label>
+            <label for="onderwerp">Onderwerp</label><input type="text" name="onderwerp" id="onderwerp"/>
+            <label for="type">Type</label>
+            <select name="type" id="type">
+                <option>IT</option>
+                <option>Elektronica</option>
+                <option>Faciliteiten</option>
+            </select>
+            <label for="prior">prioriteit</label>
+            <select name="prior" id="prior">
+                <option value="1">Kritiek(1uur)</option>
+                <option value="2">Dringend(4uur)</option>
+                <option value="3">Hoog(2dagen)</option>
+                <option value="4">Gemiddeld(1week)</option>
+                <option value="5">Laag(3 maanden)</option>
+            </select>
+            <label for="campus">Locatie</label>
+            <select name="campusId" id="campusId">
+                <option value="1">Elfde Linie</option>
+                <option value="2">Diepenbeek</option>
+                <option value="3">VilderStraat</option>
+            </select>
+            <select name="blokId" id="blokId">
+                <option value="1">A</option>
+                <option value="2">B</option>
+                <option value="3">C</option>
+                <option value="4">D</option>
+            </select>
+            <label for="lokaal">Lokaal</label><input name="lokaal" id="lokaal" type="text"/>
+            <label for="omschrijving">Omschrijving</label><textarea name="omschrijving" id="omschrijving"></textarea>
+            <label for="foto">Optioneel:foto</label>
+            <input id="foto" name="foto" type="file"/>
+            <button type="submit" name="submit" id="submit">Inzenden</button>
+        </fieldset>
+    </form>
 </div>
 </body>
 </html>
