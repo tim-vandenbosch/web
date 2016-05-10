2672453a99c06ccb33532cbd62e5a96ccdcb67b1
Daniela Lupo <daniela.lupo@student.pxl.be>
2016-05-10 17:48:57 +0200

    bestandje vergeten

---
+++ website/application/models/ticket_model.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: nida ilyas
+ * Date: 28/04/2016
+ * Time: 12:13
+ */
+class ticket_model
+{
+    var $ticketID ="";
+    var $aanmaker ="";
+    var $onderwerp="";
+    var $prioriteit="";
+    var $type="";
+    var $campusID ="";
+    var $blokID="";
+    var $lokaalNummer="";
+    var $datum="";
+    var $omschrijving="";
+    var $bijlage="";
+    var $herstellingDatum;
+    var $deadline="";
+    var $hersteller="";
+    var $status="";
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function getAll()
+    {
+        $query = $this -> db ->get('tickets');
+        return $query -> result();
+    }
+}
