03316b6f959fa929ffa2e528242145438abf82ad
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-11 15:24:35 +0200

    more fixed now i am done

--- website/application/models/ticket_model.php
+++ website/application/models/ticket_model.php
@@ -1,52 +1,53 @@
 <?php
+    /**
+     * Created by PhpStorm.
+     * User: Marnix_laptop
+     * Date: 09/05/2016
+     * Time: 09:48
+     */
+Class Ticket_model extends CI_Model{
 
-/**
- * Created by PhpStorm.
- * User: nida ilyas
- * Date: 28/04/2016
- * Time: 12:13
- */
-class ticket_model
-{
+    function getUserTickets($userID){
 
-        function getAll()
-        {
-            $query = $this->db->get('tickets');
-
-            if($query -> num_rows() >= 1)
-            {
-                return $query->result();
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        function getList()
-        {
-                $query = $this->db->get('tickets');
-                return $query->result_array();
-                /**return $query = $this->db->get('tickets');**/
-        }
-
-        function getUserTickets($userID){
-        $this -> db -> select('ticketID,onderwerp,prioriteit,type,campusID,blokID,lokaalNummer,herstellingsDatum,status');
+        $this -> db -> select('ticketID,onderwerp,prioriteit,type,campusID,blokID,lokaalNummer,herstellingDatum,status');
         $this -> db -> from('tickets');
         $this -> db -> where('aanmaker',$userID);
 
         $query = $this -> db -> get();
 
-            // Als er geen tickets gevonden worden in de db
-           if($query -> num_rows() == 1)
-            {
-                return $query->result();
-            }
-            else
-            {
-                return false;
-            }
+        // Als er geen tickets gevonden worden in de db
+        if($query -> num_rows() == 1)
+        {
+            return $query->result();
         }
+        else
+        {
+            return false;
+        }
+    }
+    function getAllTickets(){
+
+        $query = $this -> db -> get('tickets');
+
+        // Als er tickets gevonden worden in de db
+        if($query -> num_rows() >= 1)
+        {
+            return $query->result();
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    function getList()
+    {
+        $query = $this->db->get('tickets');
+        return $query->result_array();
+        /**return $query = $this->db->get('tickets');**/
+    }
+
 
     function getTicketArray($userID){
 
