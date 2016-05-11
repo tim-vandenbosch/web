92dbeaf715e4770d0bcedde61596cdf608ad0479
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-11 14:16:49 +0200

    proper coding

--- website/application/controllers/newTicket_controller.php
+++ website/application/controllers/newTicket_controller.php
@@ -16,7 +16,8 @@ class newTicket_controller extends CI_Controller
 
     function index()
     {
+        $ticketId = $this-> ticket_model ->getLastTicketId()[0]->ticketID;
         $this->load->view('newTicket_view',$data = array('test'=>5,
-                                            'ticket' => $this-> ticket_model ->getLastTicketId()));
+                                            'ticket' => $ticketId));
     }
 }
