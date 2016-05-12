546f951f4e17927df2dd380c995cb00f87a46b11
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-12 09:01:55 +0200

    Nida JUISTE klassen geven

--- website/application/views/Dispatcher/index.php
+++ website/application/views/Dispatcher/index.php
@@ -8,37 +8,43 @@
 ?>
 <div class="row">
     <div class="col-md-8 col-md-offset-2 titel">
-        <h1>Welkom Dispatcher</h1>
-        <h1>Overzicht tickets</h1>
+        <h1>Nieuw Ticket</h1>
     </div>
 </div>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2 main home ">
+        <div class="col-md-4 left">
+            <h2><!--titel--></h2>
+        </div>
+        <div class="col-md-8 right">
+            <!-- hier kan nog eventuele uitleg over overzicht-->
+        </div>
+        <div class="col-md-12 center">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Onderwerp</th>
+                    <th>Type</th>
+                    <th>Prioriteit</th>
+                    <th>Status</th>
+                    <!--            <th>Locatie </th>-->
 
-<div class="col-md-8 col-md-offset-2 main home ">
+                </tr>
+                </thead>
+                <tbody>
 
-    <table class="table table-condensed">
-        <thead>
-        <tr>
-            <th>Onderwerp</th>
-            <th>Type</th>
-            <th>Prioriteit</th>
-            <th>Status</th>
-            <!--            <th>Locatie </th>-->
-
-        </tr>
-        </thead>
-        <tbody>
-
-        <tr>
-            <td><?php echo $onderwerp;?></td>
-            <td><?php echo $type;?></td>
-            <td> <?php echo $prioriteit;?></td>
-            <td><?php echo $status;?></td>
-            <!--            <td>--><?php //echo $naam ;?><!--</td>-->
-            <td> <button type="button" class="btn btn-link" onclick=<?php echo base_url()?>"Dispatcher/details" >Details</button></td>
+                <tr>
+                    <td><?php echo $onderwerp;?></td>
+                    <td><?php echo $type;?></td>
+                    <td> <?php echo $prioriteit;?></td>
+                    <td><?php echo $status;?></td>
+                    <!--            <td>--><?php //echo $naam ;?><!--</td>-->
+                    <td> <button type="button" class="btn btn-link" onclick=<?php echo base_url()?>"Dispatcher/details" >Details</button></td>
 
 
-        </tr>
+                </tr>
 
-        </tbody>
-    </table>
+                </tbody>
+            </table
+        </div>
 </div>
