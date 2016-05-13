3f1d43adcd446a4888e4e65c300ef4bd4bc23c7e
hitocage <briittv@gmail.com>
2016-05-13 13:18:26 +0200

    Juiste admin view + deel van tabel users gemaakt

--- website/application/views/admin_view.php
+++ website/application/views/admin_view.php
@@ -5,45 +5,21 @@
  * Bron: /
  */
 ?>
-<html>
-<head>
-    <title>User ticket</title>
-    <?= link_tag('/assets/bootstrap/css/bootstrap.css') ?>
-    <?= link_tag('/assets/bootstrap/css/customStyle.css') ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body id="background">
-<header class="container hoofding">
-    <div id="pxlLine"></div>
-    <img class="col-md-2" src="<?php echo base_url();?>assets/Pictures/Logo_PXL.png" alt="PXL logo"/>
-    <nav class="nav">
-        <ul class="nav navbar-nav navbar-style">
-            <li><?php echo anchor(site_url(array('home','index')),'Overzicht'); ?></li>
-            <li class="active"><?php echo anchor(site_url(array('newTicket_controller','index')),'Nieuw ticket');?></li>
-            <li><?php echo anchor(site_url(array('profiel_controller','index')),'Profiel');?></li>
-            <li><?php echo anchor(site_url(array('home','logout')),'Afmelden'); ?></li>
-        </ul>
-    </nav>
-    <div class="col-md-2"></div>
-</header>
-<div class="row">
-    <div class="col-md-8 col-md-offset-2 titel">
-        <h1>Welkom Admin</h1>
-    </div>
+<div class="col-md-7 col-md-offset-2 titel">
+    <h1>Welkom Admin</h1>
 </div>
-<div class="row">
-    <div class="col-md-8 col-md-offset-2 main home ">
-        <div class="col-md-4 left">
-            <h2><!--titel--></h2>
-        </div>
-        <div class="col-md-8 right">
-            <!-- hier kan nog eventuele uitleg over overzicht-->
-        </div>
-        <div class="row">
-            
-        </div>
-    </div>
+
+<div class="col-md-7 col-md-offset-2 main home ">
+    <table>
+        <tr>
+            <th>UserID</th>
+            <th>Email</th>
+            <th>Rol</th>
+            <th>Actief</th>
+        </tr>
+        <?php
+            foreach (_user FROM )
+        ?>
+    </table>
 </div>
-</body>
-</html>
+
