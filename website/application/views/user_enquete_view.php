1f8095a0833ea6891a2e5526f6795f89fa4d0665
hitocage <briittv@gmail.com>
2016-05-15 14:09:25 +0200

    foreach erin zetten

--- website/application/views/user_enquete_view.php
+++ website/application/views/user_enquete_view.php
@@ -13,11 +13,10 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2 main home ">
        <form>
-           <!-- <?php foreach ($vragen as $vraag):?>
-               <?php $antwoord1=htmlentities($antwoord ->userID, ENT_QUOTES, 'UTF-8'); ?>
-                <input type="radio" name="vraag1" value="">
+           <?php foreach ($vragen as $vraag):?>
 
-           <?php endforeach; ?> -->
+
+           <?php endforeach; ?>
            <input class="btn btn-default" type="submit" value="inzenden" name="submit" id="submit"/>
        </form>
     </div>
