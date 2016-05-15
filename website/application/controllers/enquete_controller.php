0f5c0956b9dedc0f0070fa72ea87c25d7fdff277
hitocage <briittv@gmail.com>
2016-05-15 12:56:28 +0200

    Juiste model linken + views

--- website/application/controllers/enquete_controller.php
+++ website/application/controllers/enquete_controller.php
@@ -9,13 +9,15 @@ class enquete_controller extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model(vragenlijst_model);
-
+        $this->load->model('enquete_model', '', TRUE);
     }
 
     function index()
     {
-
+        $this->load->view('header');
+        $this->load->view('navigation');
+        $this->load->view('user_enquete_view');
+        $this->load->view('footer');
     }
     
 }
