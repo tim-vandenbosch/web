dae933f6bfd71a958c27e3d46ae841e70bc7f2b4
hitocage <briittv@gmail.com>
2016-05-15 16:59:41 +0200

    very last update

--- website/application/views/user_enquete_view.php
+++ website/application/views/user_enquete_view.php
@@ -18,18 +18,34 @@
             ?>
                <div class="form-group row">
                    <p> <?= htmlentities($vragen[0] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
+                   <!-- Dit kan waarschijnlijk in een loop -->
                    <input type="radio" name="vraag1"
                           value="<?php echo htmlentities($vragen[0] -> antw1_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[0] -> antw2_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[0] -> antw3_text, ENT_QUOTES, 'UTF-8') ;?>">
                </div>
 
                <div class="form-group row">
                     <p><?= htmlentities($vragen[1] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[1] -> antw1_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[1] -> antw2_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[1] -> antw3_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <input type="radio" name="vraag1"
+                          value="<?php echo htmlentities($vragen[1] -> antw4_text, ENT_QUOTES, 'UTF-8') ;?>">
                </div>
                <div class="form-group row">
                    <p><?= htmlentities($vragen[2] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
                    <textarea class="form-control" rows="3" name="feedback" id="feedback"></textarea>
+                   <!-- Moet met css nog even tegoei maar tijdelijke oplossing -->
+                   <p></p>
+                   <input class="btn btn-default" type="submit" value="inzenden" name="submit" id="submit"/>
                </div>
-               <input class="btn btn-default" type="submit" value="inzenden" name="submit" id="submit"/>
+
            </form>
         </div>
     </div>
