df6a016835b31b6587f502c60fed1a9f2c16f4e0
hitocage <briittv@gmail.com>
2016-05-15 15:29:16 +0200

    another try for making this work :hamster:

--- website/application/views/user_enquete_view.php
+++ website/application/views/user_enquete_view.php
@@ -13,12 +13,18 @@
 <div class="row">
     <div class="col-md-8 col-md-offset-2 main home ">
        <form>
-         <!--  <?php foreach ($vragen as $vraag):?>
+           <?php foreach ($vragen as $vraag):?>
                 <p><?= htmlentities($vraag -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
-                <?php foreach ($antwoorden as $antwoord):?>
-                    <input type="radio" value=<?php htmlentities($antwoord -> antw1_text, ENT_QUOTES, 'UTF-8')?>>
-               <?php endforeach; ?>
-           <?php endforeach; ?> -->
+                <?php
+               for ($i = 0; $i <4; $i++)
+               {
+                   if($antwoorden[$i] != NULL)
+                   {
+                       echo "<input type='radio' name='antw1' value='$antwoorden[$i]'>";
+                   }
+               }
+               ?>
+           <?php endforeach; ?>
            <input class="btn btn-default" type="submit" value="inzenden" name="submit" id="submit"/>
        </form>
     </div>
