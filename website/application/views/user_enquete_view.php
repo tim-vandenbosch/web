aa726145e736bb04e323584cb249cef24ce60d8b
hitocage <briittv@gmail.com>
2016-05-18 15:19:06 +0200

    laat de antwoorden van enquete zien :)

--- website/application/views/user_enquete_view.php
+++ website/application/views/user_enquete_view.php
@@ -9,34 +9,50 @@
     <div class="col-md-7 col-md-offset-2 titel">
         <h1>Hoe ervaarde u het gebruik</h1>
     </div>
-
-
-
     <div class="col-md-7 col-md-offset-2 main home ">
         <div class="col-md-7 col-md-offset-2 center ">
             <?php
-            echo form_open();
+                echo form_open();
             ?>
                <div class="form-group row">
-                   <p> <?= htmlentities($vragen[0] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
-                   <!-- Dit kan waarschijnlijk in een loop -->
-                   <input type="radio" name="vraag1"
-                          value="<?php echo htmlentities($vragen[0] -> antw1_text, ENT_QUOTES, 'UTF-8') ;?>">
-                   <input type="radio" name="vraag1"
-                          value="<?php echo htmlentities($vragen[0] -> antw2_text, ENT_QUOTES, 'UTF-8') ;?>">
-                   <input type="radio" name="vraag1"
-                          value="<?php echo htmlentities($vragen[0] -> antw3_text, ENT_QUOTES, 'UTF-8') ;?>">
+                   <label> <?= htmlentities($vragen[0] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></label></br>
+                   <?php
+                        $vraag1 = array
+                        (
+                            0 => htmlentities($vragen[0] -> antw1_text, ENT_QUOTES, 'UTF-8'),
+                            1 => htmlentities($vragen[0] -> antw2_text, ENT_QUOTES, 'UTF-8'),
+                            2 => htmlentities($vragen[0] -> antw3_text, ENT_QUOTES, 'UTF-8')
+                        );
+                   for ($i = 0; $i < 3; $i++)
+                   { ?>
+                        <label class="radio-inline"> <input type="radio" name="vraag1" value="<?= $vraag1[$i]; ?>"><?php echo $vraag1[$i]; ?></label>
+                  <?php }
+                   ?>
                </div>
 
                <div class="form-group row">
-                    <p><?= htmlentities($vragen[1] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></p>
-                   <input type="radio" name="vraag1"
+                    <label><?= htmlentities($vragen[1] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></label></br>
+                   <?php
+                       $vraag2 = array
+                       (
+                           0 => htmlentities($vragen[1] -> antw1_text, ENT_QUOTES, 'UTF-8'),
+                           1 => htmlentities($vragen[1] -> antw2_text, ENT_QUOTES, 'UTF-8'),
+                           2 => htmlentities($vragen[1] -> antw3_text, ENT_QUOTES, 'UTF-8'),
+                           3 => htmlentities($vragen[1] -> antw4_text, ENT_QUOTES, 'UTF-8'),
+                       );
+                        for($i = 0;$i<4;$i++)
+                        {
+                   ?>
+                   <label class="radio-inline"> <input type="radio" name="vraag1" value="<?= $vraag1[$i]; ?>"><?php echo $vraag1[$i]; ?></label>
+                   <?php } ?>
+
+                   <input type="radio" name="vraag2"
                           value="<?php echo htmlentities($vragen[1] -> antw1_text, ENT_QUOTES, 'UTF-8') ;?>">
-                   <input type="radio" name="vraag1"
+                   <input type="radio" name="vraag2"
                           value="<?php echo htmlentities($vragen[1] -> antw2_text, ENT_QUOTES, 'UTF-8') ;?>">
-                   <input type="radio" name="vraag1"
+                   <input type="radio" name="vraag2"
                           value="<?php echo htmlentities($vragen[1] -> antw3_text, ENT_QUOTES, 'UTF-8') ;?>">
-                   <input type="radio" name="vraag1"
+                   <input type="radio" name="vraag2"
                           value="<?php echo htmlentities($vragen[1] -> antw4_text, ENT_QUOTES, 'UTF-8') ;?>">
                </div>
                <div class="form-group row">
