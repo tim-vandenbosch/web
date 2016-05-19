7e12863295c3cbc8d30287d0c60dc872942e502a
hitocage <briittv@gmail.com>
2016-05-19 14:49:06 +0200

    enquete: Verzend knop werkt deels
    Na het drukken van verzenden wordt de user afgemeld

--- website/application/views/user_enquete_view.php
+++ website/application/views/user_enquete_view.php
@@ -11,7 +11,7 @@
     </div>
     <div class="col-md-7 col-md-offset-2 main home ">
         <div class="col-md-7 col-md-offset-2 center ">
-            <form method="post" action="enquete_controller">
+            <form method="post" action="enquete_controller/afmelden_verzenden">
                <div class="form-group row">
                    <label> <?= htmlentities($vragen[0] -> vraag_text, ENT_QUOTES, 'UTF-8'); ?></label></br>
                    <?php
