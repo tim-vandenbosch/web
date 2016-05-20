b21721bca33321d0e80735e3fec8c1a7f6e303da
hitocage <briittv@gmail.com>
2016-05-20 13:44:56 +0200

    aa bit futher, nog error

--- website/application/controllers/enquete_controller.php
+++ website/application/controllers/enquete_controller.php
@@ -38,27 +38,36 @@ class enquete_controller extends CI_Controller
         }*/
 
     }
+    // Gebruiker: Britt
+    // Datum: 20/05/2016
+
+    function  validatie()
+    {
+        $this -> form_validation -> set_rules('vraag1','Vraag1', 'required');
+        $this -> form_validation -> set_rules('vraag2','Vraag2', 'required');
+        $this -> form_validation -> set_rules('vraag3','Vraag3', 'required');
+        $this->form_validation->set_message('Gelieve alles in te vullen');
+    }
 
     // Gebruiker: Britt
     // Datum: 18/05/2016
     // Het afmelden van de docent voltooien en de gegevens verzenden
     function afmelden_verzenden()
     {
-        $ingevulde_antwoorden = array
-        (
-            "antw1" => $this -> input -> get('vraag1'),
-            "antw2" => $this -> input -> post('vraag2'),
-            "antw3" => $this -> input -> post('vraag3')
-        );
-
-
-        for($i = 1; $i < 4;$i++)
-        {
-            $this->enquete_model->voeg_antwoord($i,$ingevulde_antwoorden[$i-1]);
-        }
-        /*
-        $this->session->unset_userdata('logged_in');
-        session_destroy();
-        redirect('login', 'refresh');*/
+        //form geeft null (why?)
+            $ingevulde_antwoorden = array
+            (
+                "antw1" => $this->input->get('vraag1'),
+                "antw2" => $this->input->post('vraag2'),
+                "antw3" => $this->input->post('vraag3')
+            );
+            
+            for ($i = 1; $i < 4; $i++) {
+                $this->enquete_model->voeg_antwoord($i, $ingevulde_antwoorden[$i - 1]);
+            }
+            /*
+            $this->session->unset_userdata('logged_in');
+            session_destroy(); */
+            redirect('login', 'refresh');
     }
 }
