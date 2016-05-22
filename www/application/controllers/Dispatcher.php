17bf1a38ba3274123f25d5d1fc341a9be0213ca5
hitocage <briittv@gmail.com>
2016-05-22 15:32:29 +0200

    dispatcher controller opmaak
    Lege functies (index en getIDwerkman) + updateticket heeft geen author +
    beschrijving + date ontbreken

--- www/application/controllers/Dispatcher.php
+++ www/application/controllers/Dispatcher.php
@@ -1,97 +1,93 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: DanielaCarmelina
- * Date: 10/05/2016
- * Time: 12:32
+/* @author = Daniela Carmelina
+ * Date = 10/05/2016
  */
 class Dispatcher  extends CI_Controller
 {
-    // @author =  Nida
-//@reviewer = Daniela
 
+    /* @author =  Nida
+     */
     function __construct() {
         parent::__construct();
         $this -> checkSession();
-        $this->load->database();
-        $this->load->model('ticket_model');
-        $this->load->model('user_model');
-        // $this -> load -> library('form_validation');
+        // $this -> load -> database();
+        $this -> load -> model('ticket_model');
+        $this -> load -> model('user_model');
     }
 
-    // @author =  Daniela
-//@reviewer =
+    /* @author =  Daniela
+     */
     function settings(){
-
-        $this->load->view('Dispatcher/settings');
-
+        $this -> load -> view('Dispatcher/settings');
     }
-    // @author =  Daniela
-//@reviewer =
+    /* @author =  Daniela
+     */
     public function index()
     {
 
     }
 
-    // @author =  Daniela
-//@reviewer =
+    /* @author =  Daniela
+     */
     public function details($ticketID,$k)
     {
         $data['message'] = "";
         // $this -> db ->join('campus'sen,'tickets.campusID=campussen.campusName');
-        $data['query'] = $this-> ticket_model ->getdetailsTicket($ticketID);
+        $data['query'] = $this -> ticket_model -> getdetailsTicket($ticketID);
         $data['werkmannen'] = $this -> user_model -> getWerkmannen();
 
         $data['werkmanEmail'] = $this ->  ticket_model -> getWerkman($ticketID);
         
-        $data['stat'] = $this-> ticket_model ->getEnums("'status'");
-        $data['prio'] = $this-> ticket_model ->getEnums("'prioriteit'");
+        $data['stat'] = $this -> ticket_model -> getEnums("'status'");
+        $data['prio'] = $this -> ticket_model -> getEnums("'prioriteit'");
 
-     if($k == "update"){
+        if($k == "update")
+        {
             $data['message'] = "Ticket update is geslaagd";
         }
-        $this->load->view('Layout/header');
-        $this->load->view('Layout/navigation');
-        $this->load->view('Dispatcher/details',$data);
-        $this->load->view('Layout/footer');
-        
-
+        $this -> load -> view('Layout/header');
+        $this -> load -> view('Layout/navigation');
+        $this -> load -> view('Dispatcher/details',$data);
+        $this -> load -> view('Layout/footer');
     }
 
+    /* @author = ?
+     *
+     */
     function update($ticketID){
         $data = array(
            // 'ticketId'=>$this -> input -> post('ticketID'),
             'ticketId'=> $ticketID,
-                'prioriteit' => $this -> input -> post('dprioriteit'),
-                'herstellingDatum' => $this -> input -> post('dherstellingsdatum'),
-                'deadline' => $this -> input -> post('ddeadline'),
+            'prioriteit' => $this -> input -> post('dprioriteit'),
+            'herstellingDatum' => $this -> input -> post('dherstellingsdatum'),
+            'deadline' => $this -> input -> post('ddeadline'),
             'hersteller' => $this -> input -> post('dwerkman'),
             'status' => $this -> input -> post('dstatus')
-        
         );
-
+        
         $this -> ticket_model -> updateTicket($data);
-
         $this->details($ticketID,"update");
     }
-    
+
+    /* @author = ?
+     */
     function getIdWerkman($email){
         
     }
 
-    //bronnen : https://www.formget.com/update-data-in-database-using-codeigniter/
-    // @author =  Daniela
-//@reviewer =
+    /* @author =  Daniela
+     * Bron = https://www.formget.com/update-data-in-database-using-codeigniter/
+     */
     function sort($tablename){
-        $data = $this-> ticket_model ->sorttable($tablename);
-        $this->load->view('/Dispatcher/index', $data);
-
+        $data = $this -> ticket_model -> sorttable($tablename);
+        $this -> load -> view('/Dispatcher/index', $data);
        // return $data;
     }
 
-    //@author=marnix
-    // check of user nog in gelogd is, zoniet opnieuw inloggen
+    /* @author = Marnix
+     * Check of user nog in gelogd is, zoniet opnieuw inloggen
+     */
     function checkSession(){
         if (!$this->session->userdata('logged_in')) {
             redirect('login', 'refresh');
