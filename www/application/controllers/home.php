99e4dec75cc5b9bd636d827b657dc7bcbf856e4c
hitocage <briittv@gmail.com>
2016-05-22 13:12:34 +0200

    Opmaak

--- www/application/controllers/home.php
+++ www/application/controllers/home.php
@@ -13,8 +13,8 @@ class Home extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('user_model', '', TRUE);
-        $this->load->model('ticket_model', '', TRUE);
+        $this -> load -> model('user_model', '', TRUE);
+        $this -> load -> model('ticket_model', '', TRUE);
     }
 
     function index()
@@ -29,36 +29,36 @@ class Home extends CI_Controller
             switch ($rol){
                 case "Admin":
                     $data = array('userID' => $session_data['userID'], 'users' => $this->user_model->get_users());
-                    $this->load->view('Layout/header');
-                    $this->load->view('Layout/navigation');
-                    $this->load->view('/Admin/admin_view',$data);
-                    $this->load->view('Layout/footer');
+                    $this -> load -> view('Layout/header');
+                    $this -> load -> view('Layout/navigation');
+                    $this -> load -> view('/Admin/admin_view',$data);
+                    $this -> load -> view('Layout/footer');
                     break;
                 case "Dispatcher":
                     $data =  array('userID' => $session_data['userID'], 'tickets' => $this->ticket_model->getAllTickets());
-                    $this->load->view('Layout/header');
-                    $this->load->view('Layout/navigation');
-                    $this->load->view('/Dispatcher/index', $data);
-                    $this->load->view('Layout/footer');
+                    $this -> load -> view('Layout/header');
+                    $this -> load -> view('Layout/navigation');
+                    $this -> load -> view('/Dispatcher/index', $data);
+                    $this -> load -> view('Layout/footer');
 
                     break;
                 case "Werkman":
                     $data =  array('userID' => $session_data['userID'], 'tickets' => $this->ticket_model->getAllTickets());
-                    $this->load->view('Layout/header');
-                    $this->load->view('Layout/navigation');
-                    $this->load->view('/Werkman/index');
-                    $this->load->view('/Tickets/lijst_tickets', $data);
-                    $this->load->view('Layout/footer');
+                    $this -> load -> view('Layout/header');
+                    $this -> load -> view('Layout/navigation');
+                    $this -> load -> view('/Werkman/index');
+                    $this -> load -> view('/Tickets/lijst_tickets', $data);
+                    $this -> load -> view('Layout/footer');
                     break;
                 case "Docent":
                     $data = array('userID' => $session_data['userID'], 'tickets' => $this->ticket_model->getAllTickets());
-                    $this->load->view('Layout/header');
-                    $this->load->view('Layout/navigation');
-                    $this->load->view('user_view', $data);
-                    $this->load->view('Layout/footer');
+                    $this -> load -> view('Layout/header');
+                    $this -> load -> view('Layout/navigation');
+                    $this -> load -> view('user_view', $data);
+                    $this -> load -> view('Layout/footer');
                     break;
                 default:
-                    $this->load->view('login_view');
+                    $this -> load -> view('login_view');
                     break;
             }
 
@@ -66,9 +66,9 @@ class Home extends CI_Controller
 
     function logout()
     {
-        $session_data = $this->session->userdata('logged_in');
+        $session_data = $this -> session -> userdata('logged_in');
         $userID = $session_data['userID'];
-        $rol = $this->user_model->neem_rol($userID);
+        $rol = $this -> user_model -> neem_rol($userID);
         $enquete = $this -> user_model -> check_enquete($userID);
 
         // @Author: Britt
@@ -79,14 +79,15 @@ class Home extends CI_Controller
         }
         else
         {
-            $this->session->unset_userdata('logged_in');
+            $this -> session -> unset_userdata('logged_in');
             session_destroy();
             redirect('login', 'refresh');
         }
 
     }
-    function checkSession(){
-        if (!$this->session->userdata('logged_in')) {
+    function checkSession()
+    {
+        if (!$this -> session -> userdata('logged_in')) {
             echo "<script>alert('U sessie is verlopen!');</script>";
             redirect('login', 'refresh');
 
