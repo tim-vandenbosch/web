d8008d35424f70c5c2d3bb2366792514fab5fbf6
hitocage <briittv@gmail.com>
2016-05-22 13:56:21 +0200

    enquete model opmaken + commentaar

--- www/application/models/enquete_model.php
+++ www/application/models/enquete_model.php
@@ -1,19 +1,20 @@
 <?php
 
-/**
- * Gebruiker: Britt
- * Datum: 28/04/2016
- * Bron: /
+/* @author = Britt
+ * Date = 28/04/2016
  */
 class enquete_model extends CI_Model
 {
+    /*
+     * Neemt alle vragen met antwoord uit de db
+     */
     function get_vragen()
     {
-        $this->db->select('vragenID, vraag_text, antw1_text, antw2_text, antw3_text, antw4_text');
-        $this->db->from('vragen');
+        $this -> db -> select('vragenID, vraag_text, antw1_text, antw2_text, antw3_text, antw4_text');
+        $this -> db -> from('vragen');
 
-        $query = $this->db->get();
-        return $query->result();
+        $query = $this -> db -> get();
+        return $query -> result();
     }
 
     /*function get_antwoorden($vraagID)
@@ -26,18 +27,24 @@ class enquete_model extends CI_Model
         return $query->row();
     } */
 
+    /*
+     * Voegt de ingegeven antwoorden met bijpassende vraagID naar de db (antwoorden)
+     */
     function voeg_antwoord($vraagID, $antwoord)
     {
-        $id = $this->db->insert_id();
+        $id = $this -> db -> insert_id();
         $data = array
         (
            'vraagID' => $vraagID,
             'antwoord_text' => $antwoord
         );
 
-        $this->db->insert('antwoorden',$data);
+        $this -> db -> insert('antwoorden',$data);
     }
 
+    /*
+     * De enquetebool inzetten naar 1 ==> want de enquete is ingevuld
+     */
     function enquete_ingevuld($userID, $bool)
     {
         $this -> db -> set('enqueteBool', $bool);
