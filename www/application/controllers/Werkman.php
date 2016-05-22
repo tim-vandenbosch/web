c2749462f8564b7085631fd5c47649d291919868
hitocage <briittv@gmail.com>
2016-05-22 16:02:20 +0200

    last opmaak! =D

--- www/application/controllers/Werkman.php
+++ www/application/controllers/Werkman.php
@@ -1,72 +1,72 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nida ilyas
- * Date: 5/05/2016
- * Time: 22:56
+/* @author =  Nida
+ * Date = 5/05/2016
  */
 class Werkman extends CI_Controller
 {
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
-        $this->load->database();
-        $this->load->model('ticket_model');
-        $this->load->model('user_model');
+        $this -> load -> database();
+        $this -> load -> model('ticket_model');
+        $this -> load -> model('user_model');
     }
 
-    function showTicketsToDo(){
+    function showTicketsToDo()
+    {
         $this -> checkSession();
-        $session_data = $this->session->userdata('logged_in');
+        $session_data = $this -> session -> userdata('logged_in');
         $data['userID'] = $session_data['userID'];
         $userID = $session_data['userID'];
-        $rol = $this->user_model->neem_rol($userID);
-
-        $data =  array('userID' => $session_data['userID'], 'tickets' => $this->ticket_model->getTicketsByWm($session_data['userID']));
-        $this->load->view('Layout/header');
-        $this->load->view('Layout/navigation');
-        $this->load->view('/Werkman/index');
-        $this->load->view('/Werkman/lijst_ticketsToDo', $data);
-        $this->load->view('Layout/footer');
+        $rol = $this -> user_model -> neem_rol($userID);
+        $data =  array('userID' => $session_data['userID'], 'tickets' => $this -> ticket_model -> getTicketsByWm($session_data['userID']));
+        $this -> load -> view('Layout/header');
+        $this -> load -> view('Layout/navigation');
+        $this -> load -> view('/Werkman/index');
+        $this -> load -> view('/Werkman/lijst_ticketsToDo', $data);
+        $this -> load -> view('Layout/footer');
     }
-    function updateTicketStatus($ticketID,$k){
+
+    function updateTicketStatus($ticketID,$k)
+    {
         $data['message'] = "";
         // $this -> db ->join('campus'sen,'tickets.campusID=campussen.campusName');
-        $data['query'] = $this-> ticket_model ->getdetailsTicket($ticketID);
+        $data['query'] = $this -> ticket_model -> getdetailsTicket($ticketID);
+        $data['stat'] = $this -> ticket_model -> getEnums("'status'");
 
-        $data['stat'] = $this-> ticket_model ->getEnums("'status'");
-
-        if($k == "update"){
+        if($k == "update")
+        {
             $data['message'] = "Ticket update is geslaagd";
         }
 
-        $data['query'] = $this-> ticket_model ->getdetailsTicket($ticketID);
-        $this->load->view('Layout/header');
-        $this->load->view('Layout/navigation');
-        $this->load->view('/Werkman/updateStatus_ticket', $data);
-        $this->load->view('Layout/footer');
+        $data['query'] = $this -> ticket_model -> getdetailsTicket($ticketID);
+        $this -> load -> view('Layout/header');
+        $this -> load -> view('Layout/navigation');
+        $this -> load -> view('/Werkman/updateStatus_ticket', $data);
+        $this -> load -> view('Layout/footer');
     }
-    function update($ticketID){
+    function update($ticketID)
+    {
         $data = array(
             // 'ticketId'=>$this -> input -> post('ticketID'),
             'ticketId'=> $ticketID,
             'status' => $this -> input -> post('dstatus')
         );
-
         $this -> ticket_model -> updateTicket($data);
-
-        $this->details($ticketID,"update");
-
-        $this->load->view('Layout/header');
-        $this->load->view('Layout/navigation');
-        $this->load->view('/Werkman/updateStatus_ticket', $data);
-        $this->load->view('Layout/footer');
+        $this -> details($ticketID,"update");
+        $this -> load -> view('Layout/header');
+        $this -> load-> view('Layout/navigation');
+        $this -> load -> view('/Werkman/updateStatus_ticket', $data);
+        $this -> load -> view('Layout/footer');
     }
-    //@author=marnix
-    // check of user nog in gelogd is, zoniet opnieuw inloggen
-    function checkSession(){
+
+    /* @author = Marnix
+     * Check of user nog in gelogd is, zoniet opnieuw inloggen
+     */
+    function checkSession()
+    {
         if (!$this->session->userdata('logged_in')) {
             redirect('login', 'refresh');
         }
     }
 }
-?>
