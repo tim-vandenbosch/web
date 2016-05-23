74602e3149edabea8a96a0c786fa146a8c0c09af
Marnix <marnix.meuwissen@student.pxl.be>
2016-05-23 10:57:39 +0200

    weenween, sync error deleted all

--- www/application/models/ticket_model.php
+++ www/application/models/ticket_model.php
@@ -104,27 +104,7 @@ Class Ticket_model extends CI_Model{
     }
 
     /* @author = Marnix
-     */
-    function getTicketArray($userID){
-
-        $this -> db -> select('*');
-        $this -> db -> from('tickets');
-        $this -> db -> where('aanmaker',$userID);
-
-        $query = $this -> db -> get();
-
-        // Als er geen tickets gevonden worden in de db
-        if($query -> num_rows() == 1)
-        {
-            return $query->result();
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-    /* @author = Marnix
+     * haalt laatste ticket ID op
      */
     function getLastTicketId(){
         $this -> db -> select_max('ticketID');
