3cf55e745c2aea972ddc7c7e7f064da1d39a731e
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2016-05-23 17:12:57 +0200

    ADD navigationAdmin
    voor het toevoegen van een user

---
+++ www/application/views/Layout/navigationAdmin.php
@@ -0,0 +1,44 @@
+<body id="background">
+<header class="container hoofding">
+    <div id="pxlLine"></div>
+
+    <div class="hidden-xs hidden-sm">
+        <img class="col-md-2 " src="<?php echo base_url();?>assets/Pictures/Logo_PXL.png" alt="PXL logo"/>
+
+        <nav class="navbar">
+
+            <ul class="nav navbar-nav navbar-style">
+                <li class="active"><?php echo anchor(array('home','index'),'Overzicht'); ?></li>
+                <li><?php echo anchor(site_url(array('profiel_controller','index')),'Profiel');?></li>
+                <!-- <li><?php // echo anchor(site_url(array('enquete_controller','index')),'Enquete');?></li> -->
+                <li><?php echo anchor(site_url(array('Admin', 'newUser')),'Nieuwe User');?></li>
+                <li><?php echo anchor(site_url(array('home','logout')),'Afmelden'); ?></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="hidden-md hidden-lg">
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">PXL-Ticketing system</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><?php echo anchor(array('home','index'),'Overzicht'); ?></li>
+                    <li><?php echo anchor(site_url(array('profiel_controller','index')),'Profiel');?></li>
+                    <li><?php echo anchor(site_url(array('home','logout')),'Afmelden'); ?></li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div class="col-md-2"></div>
+
+</header>
+
