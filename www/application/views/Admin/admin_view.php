fec2d5b2a359e12abf418b2432822196ffc3a618
Tim Van Den Bosch <tim_vandenbosch@msn.com>
2016-05-23 17:19:58 +0200

    update Admin_view
    Aangepaste opmaak voor de admin

--- www/application/views/Admin/admin_view.php
+++ www/application/views/Admin/admin_view.php
@@ -8,22 +8,23 @@
 </div>
 
 <div class="col-md-7 col-md-offset-2 main home ">
-    <td> <button type="button" class="btn btn-link" onclick=><?php echo anchor(site_url(array('Admin','ToevoegenView')),'Toevoegen');?></button></td>
     <table class="table table-striped" id="table">
         <thead>
             <th>UserID</th>
             <th>Email</th>
             <th>Rol</th>
             <th>Actief</th>
-            <th>Pas aan</th>
+            <th>activeer/blokkeer</th>
+            <th>Reset Password</th>
         </thead>
         <?php foreach ($users as $user):?>
            <tbody>
-               <td><?= $userid = htmlentities($user ->userID, ENT_QUOTES, 'UTF-8'); ?></td>
-               <td><?= htmlentities($user ->email, ENT_QUOTES, 'UTF-8'); ?></td>
-               <td><?= htmlentities($user ->rol, ENT_QUOTES, 'UTF-8'); ?></td>
-               <td><?= htmlentities($user ->active, ENT_QUOTES, 'UTF-8'); ?></td>
-               <td> <button type="button" class="btn btn-link" onclick=><?php echo anchor(site_url(array('Admin','BewerkenView', $userid)),'Bewerk');?></button></td>
+               <td><?= $userid = htmlentities($user -> userID, ENT_QUOTES, 'UTF-8'); ?></td>
+               <td><?= htmlentities($user -> email, ENT_QUOTES, 'UTF-8'); ?></td>
+               <td><?= htmlentities($user -> rol, ENT_QUOTES, 'UTF-8'); ?></td>
+               <td><?= htmlentities($user -> active, ENT_QUOTES, 'UTF-8'); ?></td>
+               <td><?php echo anchor(site_url(array('Admin', 'changeStatus', $userid)),'Wijzig');?></td>
+               <td><?php echo anchor(site_url(array('Admin', 'reset', $userid)),'Reset');?></td>
            </tbody>
         <?php endforeach; ?>
     </table>
