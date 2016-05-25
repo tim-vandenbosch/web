6b73aa2e168166222845adab817ee52eeeb9ec91
Daniela Lupo <daniela.lupo@student.pxl.be>
2016-05-25 18:04:41 +0200

    Update campusmodel

--- www/application/models/campus_model.php
+++ www/application/models/campus_model.php
@@ -1,12 +1,14 @@
 <?php
 /* @author = Daniela Lupo
  *  * Date = 25/05/2016
-
- * Date = 28/04/2016
  */
 Class campus_model extends CI_Model
 {
-
+    /* @author = Daniela
+     * Date = 25/05/2016
+     * Bron = geen bron gebruikt
+     * functie die alle campussen haalt uit de tabel
+     */
     function getAllCampussen()
     {
         $this->db->select('naam,campusID');
