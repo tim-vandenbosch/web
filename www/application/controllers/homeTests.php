3bd98f241a334069c0c76df6206cf73ff51217ee
Daniela Lupo <daniela.lupo@student.pxl.be>
2016-05-26 17:10:54 +0200

    Effe testing proberen laat ze effe hier staan
    er klopt iets nie....

---
+++ www/application/controllers/homeTests.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marnix_laptop
+ * Date: 26/05/2016
+ * Time: 14:43
+ */
+class homeTests extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this -> load -> library('unit_test');
+    }
+
+    function index()
+    {
+        $this -> unit -> run('lala','lala');
+        $this -> load -> view('Test/test_view');
+    }
+}
