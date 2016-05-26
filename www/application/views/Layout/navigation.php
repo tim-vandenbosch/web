3f18cc6dd72ce73d084eb4bc34413dcfcda37f38
nida-ilyas <nida.ilyas@student.pxl.be>
2016-05-26 12:50:04 +0200

    Icoontjes :game_die:

--- www/application/views/Layout/navigation.php
+++ www/application/views/Layout/navigation.php
@@ -5,11 +5,13 @@
         <img class="col-md-2 " src="<?php echo base_url();?>assets/Pictures/Logo_PXL.png" alt="PXL logo"/>
         <nav class="navbar">
             <ul class="nav navbar-nav navbar-style">
-                <li class="active"><?php echo anchor(array('home','index'),'Overzicht'); ?></li>
-                <li><?php echo anchor(site_url(array('newTicket_controller','index')),'Nieuw ticket');?></li>
-                <li><?php echo anchor(site_url(array('profiel_controller','index')),'Profiel');?></li>
+                <li class="active"><?php echo anchor(array('home','index'),'<span class="glyphicon glyphicon-list white"></span> Overzicht'); ?></li>
+                <li><?php echo anchor(site_url(array('newTicket_controller','index')),'<span class="glyphicon glyphicon-credit-card white"></span> Nieuw ticket');?></li>
+                <li><?php echo anchor(site_url(array('profiel_controller','index')),'<span class="glyphicon glyphicon-user white"></span> Profiel');?></li>
                 <!-- <li><?php // echo anchor(site_url(array('enquete_controller','index')),'Enquete');?></li> -->
-                <li><?php echo anchor(site_url(array('home','logout')),'Afmelden'); ?></li>
+
+                <li> <?php echo anchor(site_url(array('home','logout')),'<span class="glyphicon glyphicon-log-out white"></span> Afmelden'); ?></li>
+
             </ul>
         </nav>
     </div>
