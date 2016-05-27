f58d998b5f9a443c66ef8c45d89fb55af0d127a7
Daniela Lupo <daniela.lupo@student.pxl.be>
2016-05-27 20:36:20 +0200

    PHPdoc try

--- www/application/tests/controllers/DispatcherTests.php
+++ www/application/tests/controllers/DispatcherTests.php
@@ -12,11 +12,21 @@ require_once 'Dispatcher.php';
 class DispatcherTests extends TestCase
 {
     public  $test;
+    /* @author = Daniela
+     * Date = 27/05/2016
+     * Bron =
+     * Uitleg functie
+     */
     public function setup()
     {
        $this -> test = new Dispatcher();
     }
 
+    /* @author = Daniela
+     * Date = 27/5/2016
+     * Bron =
+     * Uitleg functie
+     */
     public function testDetails(){
         $details = $this -> test -> details(5,'niks');
         $this->assertTrue($details == 5);
