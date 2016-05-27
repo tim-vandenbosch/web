7b4c339e85adc7774ccf163b21662fab54a2cb19
nida-ilyas <nida.ilyas@student.pxl.be>
2016-05-27 17:21:18 +0200

    Test: test_get_vragen

--- www/application/tests/models/enquete_modelTests.php
+++ www/application/tests/models/enquete_modelTests.php
@@ -15,5 +15,43 @@ class enquete_modelTests extends TestCase
         $this->load->library('unit_test');
 
     }
+    public function test_get_vragen(){
+    $expected = [
+        0 =>(object) ['vragenID' => '1',
+            'vraag_text' => 'Was alles duidelijk bij het ingeven van een probleem?',
+            'antw1_text'=>'Ja', 'antw2_text'=>'Soms',
+            'antw3_text'=>'Nee', 'antw4_text' =>''],
+
+
+        1 =>(object) ['vragenID' => '2', 'vraag_text' => 'Wat vindt u van de interface?',
+            'antw1_text'=>'Geweldig', 'antw2_text'=>'Duidelijk',
+            'antw3_text'=>'Neutraal', 'antw4_text' =>'Kan beter'],
+
+
+        2 =>(object) ['vragenID' => '3', 'vraag_text' => 'Heeft u nog andere opmerkingen?',
+            'antw1_text'=>'', 'antw2_text'=>'',
+            'antw3_text'=>'', 'antw4_text' =>''],
+
+    ];
+
+
+        $result = $this -> model -> get_vragen();
+
+        for($i=0;$i<count($expected);$i++){
+            $this -> assertEquals($result[$i]-> vragenID,$expected[$i]->vragenID);
+            $this -> assertEquals($result[$i]-> vraag_text,$expected[$i]->vraag_text);
+
+
+            $this -> assertEquals($result[$i]-> antw1_text,$expected[$i]->antw1_text);
+            $this -> assertEquals($result[$i]-> antw2_text,$expected[$i]->antw2_text);
+            $this -> assertEquals($result[$i]-> antw3_text,$expected[$i]->antw3_text);
+            $this -> assertEquals($result[$i]-> antw4_text,$expected[$i]->antw4_text);
+        }
+
+
+
+    }
 
 }
+//$this -> db -> select('vragenID, vraag_text, antw1_text, antw2_text, antw3_text, antw4_text');
+//Was alles duidelijk bij het ingeven van een probleem?
